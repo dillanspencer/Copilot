adb3c069a738da2a523c565b1ae07bc1b205ea86
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2022-09-18 12:37:27 -0700

    Update snake.py

--- utils/snake.py
+++ utils/snake.py
@@ -15,13 +15,13 @@ class Snake():
 
     def getMoves(self, enemySnakes=[]):
         moves = []
-        if self.head.x >= self.neck.x and Point(self.head.x + 1, self.head.y) not in self.body:          
+        if self.head.x >= self.neck.x and Point(self.head.x + 1, self.head.y) not in self.body and self.head.x < 11:          
             moves.append(Move.RIGHT)
-        if self.head.x <= self.neck.x and Point(self.head.x - 1, self.head.y) not in self.body:
+        if self.head.x <= self.neck.x and Point(self.head.x - 1, self.head.y) not in self.body and self.head.x > 0:
             moves.append(Move.LEFT)
-        if self.head.y >= self.neck.y and Point(self.head.x, self.head.y + 1) not in self.body:
+        if self.head.y >= self.neck.y and Point(self.head.x, self.head.y + 1) not in self.body and self.head.y < 11:
             moves.append(Move.UP)
-        if self.head.y <= self.neck.y and Point(self.head.x, self.head.y - 1) not in self.body:
+        if self.head.y <= self.neck.y and Point(self.head.x, self.head.y - 1) not in self.body and self.head.y > 0:
             moves.append(Move.DOWN)
 
         if enemySnakes:
