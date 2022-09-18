e91738265e664ed6de37cf6383a11285286d206b
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2022-09-18 11:48:47 -0700

    Update minmax.py

--- ai/minmax.py
+++ ai/minmax.py
@@ -67,11 +67,11 @@ def heuristic(mySnake, enemySnakes, food):
     for enemySnake in enemySnakes:
         # check if snake hit enemy body
         for point in enemySnake.body:
-            if myHead.x == point.x and myHead.y == point.y:
+            if myHead == enemySnake.head:
                 if len(enemySnake.body) >= len(mySnake.body):
-                    return math.inf
-                else:
                     return -math.inf
+                else:
+                    return math.inf
         myDistance -= myHead.distance(enemySnake.head)
     
     return myDistance
