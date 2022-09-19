d92c5ce94963c5b23fd0cdd1cb7f2ed60980d877
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2022-09-18 18:06:34 -0700

    Update minmax.py

--- ai/minmax.py
+++ ai/minmax.py
@@ -4,21 +4,6 @@ import math
 import time
 from utils.snake import Snake
 from utils.utils import Move, Point
-from multiprocessing.pool import ThreadPool
-
-class MinMaxThread(Thread):
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs)
-        self._return = None
-
-    def run(self):
-        if self._target is not None:
-            self._return = self._target(*self._args,
-                                                **self._kwargs)
-    def join(self, *args):
-        Thread.join(self, *args)
-        return self._return
 
 # iterative deepening algorithm using maxN algorithm with alpha beta pruning
 def iterativeDeepening(mySnake, enemySnakes, food, depth) -> Move:
