504c384db50b99daa44b9b4c4b7d003e35770897
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2022-09-18 17:37:57 -0700

    Update minmax.py

--- ai/minmax.py
+++ ai/minmax.py
@@ -26,7 +26,7 @@ def iterativeDeepening(mySnake, enemySnakes, food, depth) -> Move:
     startTime = time.time()
     alpha = -math.inf
     beta = math.inf
-    thread = []
+    threads = []
     for i in range(1, depth):
         moveThread = MinMaxThread(target=maxN, args=(mySnake, enemySnakes, food, i, alpha, beta, startTime))
         moveThread.start()
