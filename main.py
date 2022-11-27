d81c64a0b43959e0a2ca4ab85dbdecbc02b6a4c9
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2022-11-27 11:57:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def move(game_state: typing.Dict):
     food = [Point(game_state["board"]["food"][i]["x"], game_state["board"]["food"][i]["y"]) for i in range(len(game_state["board"]["food"]))]
 
     board = Board(11, 11, enemySnakes, food, mySnake)
-    move = iterativeDeepening(board, mySnake, enemySnakes=enemySnakes, food=food, depth=10)
+    move = iterativeDeepening(board, mySnake, enemySnakes=enemySnakes, food=food, depth=8)
     if move is None:
         move = random.choice(mySnake.getMoves(enemySnakes))
 
