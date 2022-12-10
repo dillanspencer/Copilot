1e9ed138b6d7ba09108a29bf4b0bd25e71fca2d1
Dillan <dillanspencer@uvic.ca>
2022-12-09 16:44:48 -0800

    Update minmax.py

--- ai/minmax.py
+++ ai/minmax.py
@@ -1,4 +1,4 @@
-import random
+from scipy.spatial import Voronoi
 import copy
 import math
 import time
@@ -89,4 +89,95 @@ def heuristic(mySnake, enemySnakes):
   return value
 
 
+def calculate_voronoi(snakes):
+  # Create an empty list to store the regions
+  regions = []
+  
+  # Create a list of the positions of all snakes
+  snake_positions = [snake.position for snake in snakes]
+  
+  # Create the Voronoi diagram using the scipy.spatial.Voronoi class
+  voronoi = Voronoi(snake_positions)
+  
+  # Iterate through the regions in the Voronoi diagram
+  for region in voronoi.regions:
+    # Skip empty regions
+    if region == []:
+      continue
+      
+    # Calculate the size of the region
+    size = calculate_region_size(region, voronoi.vertices)
     
+    # Find the snake associated with the region by finding the closest snake to the centroid of the region
+    snake = find_closest_snake(region, voronoi.points, snakes)
+    
+    # Create a Region object for the region and append it to the list of regions
+    regions.append(Region(snake, size))
+  
+  # Return the list of regions
+  return regions
+
+
+def calculate_region_size(region, vertices):
+  # Initialize the size to 0
+  size = 0
+  
+  # Iterate through the vertices in the region
+  for i in range(len(region) - 1):
+    # Get the coordinates of the current vertex and the next vertex
+    x1, y1 = vertices[region[i]]
+    x2, y2 = vertices[region[i + 1]]
+    
+    # Add the distance between the vertices to the size
+    size += ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+  
+  # Return the size
+  return size
+
+def find_closest_snake(region, points, snakes):
+  # Initialize the minimum distance to infinity
+  min_distance = float("inf")
+  
+  # Initialize the closest snake to None
+  closest_snake = None
+  
+  # Calculate the centroid of the region
+  centroid = calculate_centroid(region, points)
+  
+  # Iterate through the snakes
+  for snake in snakes:
+    # Calculate the distance between the snake and the centroid
+    distance = ((snake.position[0] - centroid[0])**2 + (snake.position[1] - centroid[1])**2)**0.5
+    
+    # Update the minimum distance and closest snake if the current snake is closer
+    if distance < min_distance:
+      min_distance = distance
+      closest_snake = snake
+  
+  # Return the closest snake
+  return closest_snake
+
+
+def calculate_centroid(region, points):
+  # Initialize the x and y coordinates to 0
+  x = 0
+  y = 0
+  
+  # Iterate through the vertices in the region
+  for vertex in region:
+    # Add the x and y coordinates of the vertex to the x and y coordinates
+    x += points[vertex][0]
+    y += points[vertex][1]
+  
+  # Calculate the average x and y coordinates
+  x /= len(region)
+  y /= len(region)
+  
+  # Return the centroid as a tuple (x, y)
+  return (x, y)
+
+
+class Region:
+  def __init__(self, snake, size):
+    self.snake = snake
+    self.size = size
